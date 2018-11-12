01f003a9a06965c889d8fca0f5485b8e2dfa1b52
zahrasadeq <33671943+zahrasadeq@users.noreply.github.com>
2018-11-12 11:42:20 -0700

    adding names

--- src/adc.h
+++ src/adc.h
@@ -1,3 +1,10 @@
+/// Author:        Andrew Enriquez, Rachel, Zahra, Sahchel
+// Net ID:         andrewenriquez, 
+// Date:           10/24/2018
+// Assignment:     Final Project
+//----------------------------------------------------------------------//
+
+
 #ifndef ADC_H
 #define ADC_H
 
